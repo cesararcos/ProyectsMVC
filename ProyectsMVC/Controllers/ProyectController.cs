cc429d5cd7d14aed4459060b468cc4a5af4db82e
cesararcos <42158971+cesararcos@users.noreply.github.com>
2019-03-11 20:54:14 -0500

    Initial commit

---
+++ ProyectsMVC/Controllers/ProyectController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectsMVC.Controllers
+{
+    public class ProyectController : Controller
+    {
+        // GET: Proyect
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create(Models.BindingModel.InicioProyectBindingModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string Name = model.Name;
+                string Title = model.Title;
+                string Details = model.Details;
+                double? PercentageAdvance = model.PercentageAdvance;
+                DateTime? ExpectedCompletionDate = model.ExpectedCompletionDate;
+
+                return RedirectToAction("Index");
+
+            }
+
+            return View(model);
+
+
+
+
+        }
+
+
+
+    }
+}
---
+++ ProyectsMVC/Models/ViewModel/ProyectViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectsMVC.Models.ViewModel
+{
+    public class InicioProyectViewModel
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public double? PercentageAdvance { get; set; }
+        public DateTime? ExpectedCompletionDate { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+
+
+    }
+}
