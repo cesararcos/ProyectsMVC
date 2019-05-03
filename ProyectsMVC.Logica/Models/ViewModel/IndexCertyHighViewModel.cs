cfd23320a09c687863d4f411371c6528e0add6cc
cesararcos <42158971+cesararcos@users.noreply.github.com>
2019-05-02 23:42:27 -0500

    vista de pruebas (prueba en cuadro)

---
+++ ProyectsMVC.Logica/Models/ViewModel/IndexCertyHighViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectsMVC.Logica.Models.ViewModel
+{
+    public class IndexCertyHighIndexViewModel
+    {
+        [Display(Name = "Descripcion nivel")]
+        public int PrueCodigo { get; set; }
+
+        [Display(Name = "Descripcion nivel")]
+        public string NivelDescripcion { get; set; }
+
+        [Display(Name = "Descripcion prueba")]
+        public string PruebaDescripcion { get; set; }
+    }
+}
---
+++ ProyectsMVC/Controllers/IndexCertyHighController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectsMVC.Controllers
+{
+    public class IndexCertyHighController : Controller
+    {
+        [Authorize]
+        // GET: IndexCertyHigh
+        public ActionResult Index(int? id)
+        {
+            Logica.BL.IndexCertyHigh pruebas = new Logica.BL.IndexCertyHigh();
+            var prueba = pruebas.GetIndexCertyHigh(id, null).FirstOrDefault();
+
+            var indexCertyHighIndexViewModel = new Logica.Models.ViewModel.IndexCertyHighIndexViewModel
+            {
+                NivelDescripcion = prueba.Niveles.niveDescripcion,
+                PruebaDescripcion = prueba.prueDescripcion
+            };
+
+            
+
+            return View(indexCertyHighIndexViewModel);
+        }
+    }
+}
