b6d7709f75a127c991d7de97e697ee16ab8da539
cesararcos <42158971+cesararcos@users.noreply.github.com>
2019-05-13 19:27:10 -0500

    Scallfold Hacer Preguntas

--- ProyectsMVC/Controllers/tbPreguntasController.cs
+++ ProyectsMVC/Controllers/tbPreguntasController.cs
@@ -10,117 +10,115 @@ using ProyectsMVC.DAL.Models;
 
 namespace ProyectsMVC.Controllers
 {
-    public class tbPruebasController : Controller
+    public class tbPreguntasController : Controller
     {
         private ProyectsMVCEntities db = new ProyectsMVCEntities();
 
-        // GET: tbPruebas
+        // GET: tbPreguntas
         public ActionResult Index()
         {
-            var tbPrueba = db.tbPrueba.Include(t => t.tbNiveles);
-            return View(tbPrueba.ToList());
+            var tbPreguntas = db.tbPreguntas.Include(t => t.tbPrueba);
+            return View(tbPreguntas.ToList());
         }
 
-        // GET: tbPruebas/Details/5
+        // GET: tbPreguntas/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            tbPrueba tbPrueba = db.tbPrueba.Find(id);
-            if (tbPrueba == null)
+            tbPreguntas tbPreguntas = db.tbPreguntas.Find(id);
+            if (tbPreguntas == null)
             {
                 return HttpNotFound();
             }
-            return View(tbPrueba);
+            return View(tbPreguntas);
         }
 
-        // GET: tbPruebas/Create
+        // GET: tbPreguntas/Create
         public ActionResult Create()
         {
-            ViewBag.niveCodigo = new SelectList(db.tbNiveles, "niveCodigo", "niveDescripcion");
+            ViewBag.prueCodigo = new SelectList(db.tbPrueba, "prueCodigo", "prueNombre");
             return View();
         }
 
-        // POST: tbPruebas/Create
+        // POST: tbPreguntas/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "prueCodigo,niveCodigo,prueNombre")] tbPrueba tbPrueba)
+        public ActionResult Create([Bind(Include = "pregCodigo,pregDescripcion,prueCodigo")] tbPreguntas tbPreguntas)
         {
             if (ModelState.IsValid)
             {
-                db.tbPrueba.Add(tbPrueba);
+                db.tbPreguntas.Add(tbPreguntas);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            ViewBag.niveCodigo = new SelectList(db.tbNiveles, "niveCodigo", "niveDescripcion", tbPrueba.niveCodigo);
-            return View(tbPrueba);
+            ViewBag.prueCodigo = new SelectList(db.tbPrueba, "prueCodigo", "prueNombre", tbPreguntas.prueCodigo);
+            return View(tbPreguntas);
         }
 
-        // GET: tbPruebas/Edit/5
+        // GET: tbPreguntas/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            tbPrueba tbPrueba = db.tbPrueba.Find(id);
-            if (tbPrueba == null)
+            tbPreguntas tbPreguntas = db.tbPreguntas.Find(id);
+            if (tbPreguntas == null)
             {
                 return HttpNotFound();
             }
-            ViewBag.niveCodigo = new SelectList(db.tbNiveles, "niveCodigo", "niveDescripcion", tbPrueba.niveCodigo);
-            return View(tbPrueba);
+            ViewBag.prueCodigo = new SelectList(db.tbPrueba, "prueCodigo", "prueNombre", tbPreguntas.prueCodigo);
+            return View(tbPreguntas);
         }
 
-        // POST: tbPruebas/Edit/5
+        // POST: tbPreguntas/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "prueCodigo,niveCodigo,prueNombre")] tbPrueba tbPrueba)
+        public ActionResult Edit([Bind(Include = "pregCodigo,pregDescripcion,prueCodigo")] tbPreguntas tbPreguntas)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(tbPrueba).State = EntityState.Modified;
+                db.Entry(tbPreguntas).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            ViewBag.niveCodigo = new SelectList(db.tbNiveles, "niveCodigo", "niveDescripcion", tbPrueba.niveCodigo);
-            return View(tbPrueba);
+            ViewBag.prueCodigo = new SelectList(db.tbPrueba, "prueCodigo", "prueNombre", tbPreguntas.prueCodigo);
+            return View(tbPreguntas);
         }
 
-        // GET: tbPruebas/Delete/5
+        // GET: tbPreguntas/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            tbPrueba tbPrueba = db.tbPrueba.Find(id);
-            if (tbPrueba == null)
+            tbPreguntas tbPreguntas = db.tbPreguntas.Find(id);
+            if (tbPreguntas == null)
             {
                 return HttpNotFound();
             }
-            return View(tbPrueba);
+            return View(tbPreguntas);
         }
 
-        // POST: tbPruebas/Delete/5
+        // POST: tbPreguntas/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            tbPrueba tbPrueba = db.tbPrueba.Find(id);
-            db.tbPrueba.Remove(tbPrueba);
+            tbPreguntas tbPreguntas = db.tbPreguntas.Find(id);
+            db.tbPreguntas.Remove(tbPreguntas);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-
-
 
         protected override void Dispose(bool disposing)
         {
